f2e16a6dbd594939c5f6e94a81008005c2ff7aee
akimmig <info@it-kimmig.de>
2021-06-07 18:04:11 +0200

    v0.9.4 ASV-BW Exportvorlage hinzugefügt Testübersicht für Schule hinzugefügt Versand an Schüler ohne Gruppe

--- src/test/resolver/test.query.ts
+++ src/test/resolver/test.query.ts
@@ -2,6 +2,9 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Test } from '../models/Test';
 import { TestService } from '../test.service';
 import { UUID } from '../../global/scalars/UUID';
+import {SchuleService} from '../../schule/schule.service'
+import {HttpException} from '@nestjs/common'
+import {checkPassword} from '../../generate'
 
 @Resolver(() => Test)
 export class TestQuery {
@@ -14,5 +17,23 @@ export class TestQuery {
     @Args('id') id: UUID,
   ): Promise<Test> {
     return await this.service.findOneById(id) as Test;
+  }
+
+  @Query(() => [Test], { nullable: true })
+  async Tests(
+    @Args('dienststelle', { type: () => String, nullable: false }) dienststelle: string,
+    @Args('passwort', { type: () => String, nullable: false }) passwort: string,
+  ): Promise<Test[]> {
+    const s = new SchuleService();
+    const tmp = await s.find({ dienststelle, unconfirmed: { $exists: false } });
+
+    if (tmp.length === 0) throw new HttpException('Keine Schule gefunden', 404);
+    if (tmp.length > 1) throw new HttpException('Mehrere Einträge gefunden', 404);
+
+    if (!checkPassword(passwort, (tmp[0] as undefined as any).passwort)) {
+      throw new HttpException('AccessDenied', 403);
+    }
+
+    return this.service.find({ _schule: tmp[0]._id });
   }
 }
